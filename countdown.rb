87229c1deea6ae255d9b2bc0c07c6d4636c6d4d5
Cristian Deleon <libaration@gmail.com>
2020-07-07 01:49:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(x)
   end
   puts "HAPPY NEW YEAR"
 end
+
+def countdown_with_sleep(x)
+  while x != 0
+    x -= 1
+    puts "#{x} SECOND(S)!"
+    sleep.1
+  end
+  puts "HAPPY NEW YEAR"
+end
